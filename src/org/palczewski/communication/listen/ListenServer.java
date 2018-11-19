1ea646645c8154cf0785efe601454ecbd10b9eff
Patrick Palczewski <psykiatris@gmail.com>
2018-11-19 14:49:09 -0800

    Attempted to create a JDK9+ module style and messed everyting up. Redoing this.

---
+++ src/org/palczewski/communication/listen/ListenServer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018.  Author: Patrick Palczewski <psykiatris@gmail.com>. Licensed under GPL 3. See LICENSE for details.
+ */
+
+package org.palczewski.communication.listen;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+* This class will listen for incoming connections, and creates threads
+* for each one.
+* */
+public class ListenServer {
+    private static final long serialVersionUID = 1L;
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
